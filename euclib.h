7759dac70b49f3f7dc13f36484aea2379dcf26b4
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-09-06 14:40:45 +0300

    colors

--- euclib.h
+++ euclib.h
@@ -1,9 +1,18 @@
 #ifndef EUCLIB_H
 #define EUCLIB_H
 
-typedef void (*graph_value_callback_t)(const float x);
-
 typedef unsigned int color_t;
+
+#define EUCLIB_WHITE    ((color_t)(0xffffffff))
+#define EUCLIB_BLACK    ((color_t)(0xff000000))
+#define EUCLIB_RED      ((color_t)(0xff3440eb))
+#define EUCLIB_GREEN    ((color_t)(0xff34eb40))
+#define EUCLIB_BLUE     ((color_t)(0xffeb3440))
+#define EUCLIB_PURPLE   ((color_t)(0xffeb34eb))
+#define EUCLIB_YELLOW   ((color_t)(0xff34ebeb))
+#define EUCLIB_ORANGE   ((color_t)(0xff3486eb))
+
+typedef float (*graph_value_callback_t)(const float x);
 
 typedef struct
 {
@@ -39,6 +48,22 @@ typedef struct
 void euclib_plot_2d_line(euclib_plot_t *plot, graph_value_callback_t callback);
 
 #ifdef EUCLIB_IMPLEMENTATION
+
+void euclib_plot_2d_bar(euclib_plot_t *plot, float values[], unsigned int count) {
+
+}
+
+void euclib_plot_2d_dots(euclib_plot_t *plot, float values[], unsigned int count) {
+
+}
+
+void euclib_plot_2d_line(euclib_plot_t *plot, graph_value_callback_t callback) {
+    for(int i = 0; i < plot->width; ++i) {
+        for(int j = 0; j < plot->height; ++j) {
+            plot->value[i + j*plot->width] = EUCLIB_ORANGE;
+        }
+    }
+}
 
 #endif
 
--- examples/example_0.c
+++ examples/example_0.c
@@ -2,9 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main()
-{
-    printf("Poggers !\n");
+#include "stb_image_write.h"
+
+#define EUCLIB_IMPLEMENTATION
+#include "euclib.h"
+
+int main() {
+    euclib_plot_t plot = {
+        .value = malloc(800 * 600 * sizeof(color_t)),
+        .width = 800,
+        .height = 600,
+    };
+
+    euclib_plot_2d_line(&plot, &sinf);
+
+    stbi_write_jpg("plot.png", 800, 600, 4, plot.value, 800);
 
     return 0;
 }
